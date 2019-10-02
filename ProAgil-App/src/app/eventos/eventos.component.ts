bac476e6e970f8df13ea9a0653c667f0e9e640b2
Leandro Rauseo <leandro.rauseo@hotmail.com>
2019-10-01 21:08:16 -0300

    Interpolação, Diretivas e Binding

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,11 +8,36 @@ import { HttpClient } from '@angular/common/Http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  eventos: any;
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor :string){
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
   getEventos() {
--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -46,6 +46,7 @@ namespace ProAgil.WebAPI
             }
 
             // app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
